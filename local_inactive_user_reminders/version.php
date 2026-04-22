613ccc8776ee01076e56828c1fd6bc21a51f6159
Gabry <47448738+gabry1975@users.noreply.github.com>
2026-04-22 16:14:00 +0200

    Delete local_inactive_user_reminders directory

--- local_inactive_user_reminders/version.php
+++
@@ -1,8 +0,0 @@
-<?php
-defined('MOODLE_INTERNAL') || die();
-
-$plugin->component = 'local_inactive_user_reminders';
-$plugin->version   = 2026022700;
-$plugin->requires  = 2021051700; // Moodle 3.11+
-$plugin->maturity  = MATURITY_ALPHA;
-$plugin->release   = '1.3';
